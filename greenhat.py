6106297ca2c59968f70638391787faa185526e1d
adre <adrekoval@gmail.com>
2014-11-16 00:00:00 -0400

    update

--- greenhat.py
+++ greenhat.py
@@ -34,7 +34,7 @@ def main(argv):
 		i += 1
 
 if __name__ == "__main__":
-	curdate = date(2014, 11, 9).strftime("%a %b %d %X %Y %z -0400")
+	curdate = date(2014, 11, 16).strftime("%a %b %d %X %Y %z -0400")
 	num_commits = randint(4, 9)
 	for commit in range(0, num_commits):
 		subprocess.call("echo '" + curdate + str(randint(0, 1000000)) +"' > realwork.txt; git add -A; GIT_AUTHOR_DATE='" + curdate + "' GIT_COMMITTER_DATE='" + curdate + "' git commit -m 'update'; git push;", shell=True)
